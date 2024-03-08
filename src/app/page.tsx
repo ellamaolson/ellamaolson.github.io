280da994fcfdcd7c190d1078cd79bc0be9495e23
Elana Olson <elanaolson@gmail.com>
2024-03-08 11:35:20 -0800

    update home page with what i'm focussing on right now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { useState } from 'react';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import Typography from '@mui/material/Typography';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -22,18 +22,16 @@ export default function Home() {
   const [value, setValue] = useState(0);
   return (
     <>
-      <Typography variant="h5" gutterBottom>
+      <Typography variant="h5" gutterBottom sx={{ marginBottom: '32px' }}>
         hiya, my name is Elana
       </Typography>
-      <Typography paragraph variant="body1" sx={{ marginTop: '22px' }}>
-        I&apos;m a sunshine chasing, wave tumbling, watercolor painting, yoga pretzeling,
-        relationship enriching, coder who is passionate about learning, problem solving, and making
-        hard concepts easier to understand for others.
+      <Typography variant="body1" gutterBottom sx={{ marginBottom: '16px' }}>
+        I&apos;m a sunshine chasing, wave tumbling, travel enthusiast, yogi, coder who is passionate
+        about learning, problem solving, and making hard concepts easier to understand for others.
       </Typography>
-      <Typography paragraph variant="body1">
-        I strike a balance between my analytical side and a more creative and outdoorsy side through
-        painting, reading, crochet, journaling, swimming, rock climbing, hiking, camping, and
-        hanging out with people that fill my cup.
+      <Typography variant="body1">
+        I love painting, reading, crochet, journaling, swimming, rock climbing, hiking, camping,
+        spending time with people that fill my cup, and jetting off on the next adventure!
       </Typography>
       <div className="columns-2 sm:columns-3 gap-4 my-8">
         <div className="relative h-40 mb-4">
@@ -97,7 +95,51 @@ export default function Home() {
           />
         </div>
       </div>
-      <Typography variant="subtitle1">Let&apos;s connect</Typography>
+
+      <Typography variant="h6" gutterBottom>
+        what am I focussing on right now?
+      </Typography>
+      <List sx={{ listStyleType: 'disc', listStylePosition: 'inside', marginBottom: '32px' }}>
+        <ListItem sx={{ display: 'list-item' }}>
+          <strong>Womens&apos; Communities</strong> - cultivating supportive environments for women
+          <List sx={{ listStyleType: 'circle', listStylePosition: 'inside' }}>
+            <ListItem sx={{ display: 'list-item' }}>
+              My good friend Ali and I started a womens group back in August 2023 to build a
+              community of women supporting women. We strive to create an environment of enrichment,
+              growth, love, and creativity for women identifying individuals locally
+            </ListItem>
+            <ListItem sx={{ display: 'list-item' }}>
+              Alongside this group, we started a bookclub to read all the wonderful books being
+              recommended in womens group. We read books written by or surrounding the topic of
+              women.
+            </ListItem>
+          </List>
+        </ListItem>
+        <ListItem sx={{ display: 'list-item' }}>
+          <strong>Health</strong> - applying new exercise and sleeping routine protocols
+        </ListItem>
+        <ListItem sx={{ display: 'list-item' }}>
+          <strong>Art</strong> - going to try ceramics again soon!
+        </ListItem>
+        <ListItem sx={{ display: 'list-item' }}>
+          <strong>Learning</strong> - too many books and podcasts, but here&apos;s some recs
+          <List sx={{ listStyleType: 'circle', listStylePosition: 'inside' }}>
+            <ListItem sx={{ display: 'list-item' }}>
+              <i>Huberman Labs</i> - a health and science podcast led by Andrew Huberman to bring
+              scientific findings and tools to the general public
+            </ListItem>
+            <ListItem sx={{ display: 'list-item' }}>
+              <i>Us: Getting Past You and Me to Build a More Loving Relationship</i> - Terrance Real
+              approaches realtional dynamimcs with an &quot;us&quot; way of thinking, instead of
+              handling lifes challenges from a &quot;you&quot; and &quot;me&quot; perspective
+            </ListItem>
+            <ListItem sx={{ display: 'list-item' }}>
+              <i>Lying</i> - In such a little book, Sam Harris discusses the morality of lying and
+              breaks down various applications and ethical dilemmas surrounding it
+            </ListItem>
+          </List>
+        </ListItem>
+      </List>
       <Box sx={{ '& > :not(style)': { m: 1 }, display: 'flex', justifyContent: 'space-between' }}>
         <Fab aria-label="github" href="https://github.com/ellamaolson" size="small">
           <GitHubIcon />
