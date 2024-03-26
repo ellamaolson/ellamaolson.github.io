47cbb22ec92a736ed6a0c88cf898fa8fc75fd204
Elana Olson <elanaolson@gmail.com>
2024-03-26 12:29:05 -0700

    next configure a basePath for assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
+  basePath: '/ellamaolson.github.io',
+};
 
 export default nextConfig;
