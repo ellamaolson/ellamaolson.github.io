b9ea2055fa2ff8362353e09a531177039f8fdc19
Elana Olson <elanaolson@gmail.com>
2024-03-26 13:16:45 -0700

    follow up configurations to next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
   basePath: '/ellamaolson.github.io',
+  images: {
+    unoptimized: true, // <==  disable server-based image optimization
+  },
 };
 
 export default nextConfig;
