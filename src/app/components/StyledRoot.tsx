bb798d763e9b332402d1f85023a490d7862c0363
Elana Olson <elanaolson@gmail.com>
2024-03-27 10:35:48 -0700

    make root container layout responsive

--- src/app/components/StyledRoot.tsx
+++ src/app/components/StyledRoot.tsx
@@ -28,7 +28,7 @@ export function StyledRoot({
           component="main"
           maxWidth="md"
           disableGutters
-          sx={{ mt: 8, mb: 2, paddingLeft: '100px', paddingRight: '100px' }}
+          sx={{ mt: 8, mb: 2, paddingLeft: '5%', paddingRight: '5%' }}
         >
           <NavigationBar />
           {children}
