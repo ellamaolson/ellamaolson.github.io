2766b62db72a28cad7b63e187c30211eddb4707e
Elana Olson <elanaolson@gmail.com>
2025-10-13 13:59:38 +0200

    update social links and work content

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,9 +1,17 @@
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
+import Fab from '@mui/material/Fab';
+import Link from '@mui/material/Link';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+
+import ArticleIcon from '@mui/icons-material/Article';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import XIcon from '@mui/icons-material/X';
+
 import workData from './work.json';
 
 export default function Work() {
@@ -20,7 +28,7 @@ export default function Work() {
   return (
     <>
       <Typography variant="h1">Resume</Typography>
-      <Box sx={{ width: '100%' }}>
+      <Box sx={{ width: '100%' }} className="mb-8">
         <Stack
           direction={'column'}
           spacing={{ xs: 1, sm: 2, md: 4 }}
@@ -52,10 +60,31 @@ export default function Work() {
                     );
                   })}
                 </List>
+                {work.relavantLinks?.map((link, index) => {
+                  return (
+                    <Link href={link} underline="hover" color="inherit" key={index}>
+                      <Typography variant="h3">{link}</Typography>
+                    </Link>
+                  );
+                })}
               </div>
             );
           })}
         </Stack>
+      </Box>
+      <Box sx={{ '& > :not(style)': { m: 1 }, display: 'flex', justifyContent: 'space-between' }}>
+        <Fab aria-label="github" href="https://github.com/ellamaolson" size="small">
+          <GitHubIcon />
+        </Fab>
+        <Fab aria-label="linkedIn" href="https://www.linkedin.com/in/elanaolson" size="small">
+          <LinkedInIcon />
+        </Fab>
+        <Fab aria-label="Medium" href="https://medium.com/@elanaolson" size="small">
+          <ArticleIcon />
+        </Fab>
+        <Fab aria-label="twitter" href="https://twitter.com/elanathellama" size="small">
+          <XIcon />
+        </Fab>
       </Box>
     </>
   );
